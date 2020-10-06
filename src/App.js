50e2dc452cd31072095ca38b3764e69a83f84dd2
MateuszMyrcik <matimyrcik@gmail.com>
2020-10-06 22:52:40 +0200

    SF-1 Add: Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello on Space Fact!</p>
+    </div>
+  );
+}
+
+export default App;
